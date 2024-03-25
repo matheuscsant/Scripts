5e827c6ebe6839cd7d805437ae7768847e474a4b
Matheus Campanhola <matheuscampanholasantos123@gmail.com>
2024-03-25 18:04:13 -0300

    git-pull
    - Para usar basta adicionar esse bat na pasta raiz onde estão os repositórios e executar, ele vai atualizar todos os repositórios que encontrar

---
+++ git-pull-all.bat
@@ -0,0 +1,20 @@
+@echo off
+
+for /d %%d in (*) do (
+	cd %%d
+	if exist .git (
+		echo ### [UPDATING...] Atualizando projeto %%d
+		echo.
+		git pull
+		echo.
+		echo ### [UPDATED] Projeto %%d atualizado
+		echo.
+	) else (
+		echo.
+		echo ### [IGNORING...] Ignorando pasta %%d
+		echo.
+	)
+	cd ..
+)
+echo [FINALIZED] Atualizacao finalizada
+pause
